52aa965cc0e7e8988c649fa8ba87995b34e95f7a
CreeperSan <admin@creepersan.com>
2018-10-17 23:04:12 +0800

    fix some bug

---
+++ config.js
@@ -0,0 +1,14 @@
+const LOCATION_CHINA_MAINLAND   = 'zh-cn';
+const LOCATION_JAPAN            = 'ja-jp';
+
+module.exports = {
+
+    // Database Config
+    DATABASE_HOST       : 'localhost',
+    DATABASE_USER       : 'creepersan',
+    DATABASE_PASSWORD   : '12345678',
+    DATABASE_NAME       : 'BingImage',
+
+    // Application Config
+    APP_LOCATION        : LOCATION_CHINA_MAINLAND
+};
